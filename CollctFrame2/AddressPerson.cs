e17e05232e557d34d3f723e79dc694f99eafb27f
Harry <hareshwarp649>
2022-03-16 12:34:54 +0530

    Collection Method to Search Person ,city across multiple address book

---
+++ CollctFrame2/AddressPerson.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollctFrame2
+{
+    internal class AddressPerson
+    {
+
+        public HashSet<GetContact> People;
+
+
+        public AddressPerson()
+        {
+            People = new HashSet<GetContact>();
+        }
+
+
+        public GetContact FindContact(string fname)
+        {
+            GetContact contact = null;
+
+            foreach (var person in People)
+            {
+                if (person.FirstName.Equals(fname))
+                {
+                    contact = person;
+                    break;
+                }
+            }
+            return contact;
+        }
+
+
+        public bool AddContact(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNumber, string Email)
+        {
+            GetContact contact = new GetContact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email);
+
+            GetContact result = FindContact(FirstName);
+
+            if (result == null)
+            {
+                People.Add(contact);
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+        public bool RemoveContact(string name)
+        {
+            //creation of object for contact
+            GetContact c = FindContact(name);
+
+            if (c != null)
+            {
+                People.Remove(c);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CollctFrame2/SearchPersonAddress.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollctFrame2
+{
+    internal class SearchPersonAddress
+    {
+        public Dictionary<string, HashSet<GetContact>> Binder = new Dictionary<string, HashSet<GetContact>>();
+        //creates a list 
+        public List<GetContact> City = new List<GetContact>();
+        //create a dictionary to store details of city
+        public Dictionary<string ,List<GetContact>> CityDictionary = new Dictionary<string, List<GetContact>>();
+
+       
+        public HashSet<GetContact> AddAddrBook(string key, HashSet<GetContact> set)
+        {
+            if (this.Binder.ContainsKey(key))
+            {
+                Console.WriteLine("Address book already exists");
+                return Binder[key];
+            }
+            else
+            {
+                Console.WriteLine("New address book created");
+                Binder.Add(key, set);
+                return Binder[key];
+            }
+        }
+
+       
+        public List<GetContact> SortingCity(string cityname)
+        {
+            //traversing in binder class
+            foreach (var key in Binder.Keys)
+            {
+               
+                foreach (GetContact c in Binder[key])
+                {
+                    if (c.City == cityname)
+                        City.Add(c);
+                }
+            }
+            return City;
+        }
+
+       
+        public List<GetContact> SearchContactsByCity(string city)
+        {
+            CityDictionary[city] = SortingCity(city);
+            return CityDictionary[city];
+        }
+    }
+}
